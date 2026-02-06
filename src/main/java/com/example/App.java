69c0d370ec48362ee0c6466f41cf7eaa98913dd3
HanJeongmok <wjdahe1845@gmail.com>
2026-02-06 12:37:08 +0900

    ch13 Red&Green

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,7 +14,8 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        int lastId=0;
+        int lastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         while (true) {
             System.out.println("명령) ");
             String cmd = sc.nextLine();
@@ -23,7 +26,19 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
                     lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
                 }
                 case "종료" -> {
                     return;
