4efade28acb556c5aff2315b881f7f2010390678
HanJeongmok <wjdahe1845@gmail.com>
2026-02-06 11:22:52 +0900

    ch.7 Red

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,18 @@
-public class AppTest
-{
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+public class AppTest {
+    @Test
+    @DisplayName("'== 명언앱 =='출력")
+    void t1(){
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
+        new App(sc).run();
+        String out = outputStream.toString();
+        Assertions.assertThat(out).contains("== 연언 앱 ==");
+    }
 }
