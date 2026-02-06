a51995c80278544e6e9ba133cbd3ffec3cdb7d0c
HanJeongmok <wjdahe1845@gmail.com>
2026-02-06 14:05:32 +0900

    ch14 15 Bule

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,42 +5,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
     private Scanner sc;
+    private WiseSayingController wiseSayingController;
+    private SystemController systemController;
 
     public App(Scanner sc) {
         this.sc = sc;
+        this.wiseSayingController=new WiseSayingController(sc);
+        this.systemController=new SystemController();
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        int lastId = 0;
-        List<WiseSaying> wiseSayings = new ArrayList<>();
-        while (true) {
-            System.out.println("명령) ");
-            String cmd = sc.nextLine();
-            switch (cmd) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String saying = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-                    lastId++;
-                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
-                    wiseSayings.add(wiseSaying);
-                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-                }
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
 
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
-                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
-                }
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> wiseSayingController.actionAdd();
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
+                    systemController.actionExit();
                     return;
                 }
             }
