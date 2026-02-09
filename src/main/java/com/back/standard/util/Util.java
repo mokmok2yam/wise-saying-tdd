6869ebcdc6e47f5c4ffab3f2ae5982e93f7cd551
HanJeongmok <wjdahe1845@gmail.com>
2026-02-09 14:37:49 +0900

    33 red

---
+++ src/main/java/com/back/standard/util/Util.java
@@ -0,0 +1,18 @@
+package com.back.standard.util;
+
+public class Util {
+    // 이너 클래스
+    public static class file {
+
+        public static void touch(String filePath) {
+        }
+
+        public static boolean exists(String filePath) {
+            return false;
+        }
+    }
+
+    public static class json {
+
+    }
+}
